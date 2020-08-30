b24ffe18184b02e8841a26634c83e48a47c28095
maliozer <mehmetali.ozer@std.yeditepe.edu.tr>
2020-08-30 23:52:02 +0300

    slurm job python helloworld commit

---
+++ example_jobs/helloworld_python/main.py
@@ -0,0 +1,17 @@
+import sys
+
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+
+
+print("Yeditepe_UBY", "hello truba, python version : ")
+print(sys.version, __name__)
+
+numbers = np.array([1, 2, 3])
+print(numbers)
+
+for x in tqmd(range(100000)):
+    pass
+
+print("END OF JOB!")
